59d65749fb7ff584f6efd574caa39532cc6853ad
Heuler Manfredi <felipemanfredigalaxy@gmail.com>
2022-12-09 22:37:54 -0300

    first commit

---
+++ src/test/java/com/javacloudstack/pro/ProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacloudstack.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
